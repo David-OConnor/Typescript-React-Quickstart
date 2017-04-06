dcf837098f284791651d7ae785a1daa142fae575
David-OConnor <david.alan.oconnor@gmail.com>
2017-04-06 08:27:47 +0200

    First commit

---
+++ src/additional_file.ts
@@ -0,0 +1,3 @@
+export function addOne(value: number) {
+    return value + 1
+}
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Importing an npm module
+import * as React from "react"
+
+// Importing another source file from our project
+import * as additional from './additional_file'
+
+alert("Five plus One is " + additional.addOne(5))
